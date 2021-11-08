b148c140b538c59e4e20d20d3e7b66aa86101e5e
andres.montoya <andres.montoya@iesonline.com.co>
2021-11-08 16:31:41 -0500

    feat: se agrega la dependencia morgan para intentar tener un mejor log

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+#!/usr/bin/env node
+
 'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
 const http = require('http');
+const morgan = require('morgan')
 
 const fs = require('fs');
 
@@ -18,6 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw());
 app.use(bodyParser.text())
 
+app.use(morgan('combined'))
+
 let recibidos = [];
 
 
@@ -25,15 +30,28 @@ app.post('/report', (req, res) => {
     const body = req.body
     console.log(body);
     recibidos.push(body)
+    console.log("-----------")
     res.send('Ok');
   });
 
   app.get('/records', (req, res) => {
+    console.log("-----------")
     res.send(recibidos);
   });
 
 app.get('/', (req, res) => {
+  console.log("-----------")
   res.send('Hello World');
+});
+
+app.use(function(err, req, res, next) {
+  if(err){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  }
+  console.log("-----------")
+  console.log(req)
+  next();
 });
 
 //app.listen(PORT, HOST);
